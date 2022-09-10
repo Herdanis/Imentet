96be1c006f810fcac167dc4791db0ed2c221556a
Herdanis <herdanis.pradana@gmail.com>
2022-09-10 15:29:44 +0700

    update init script

--- init.sh
+++ init.sh
@@ -1,4 +1,10 @@
-#!/bin/sh
-mkdir ./app
+#!/usr/bin/env bash
 
-for i in $( ls -d *.dev.* */*.dev.* ); do cp -v -- $i ${i%.dev.*}.${i##*.}; done
+for i in $(ls -d *.dev.* */*.dev.*); do
+    cp -v -- $i ${i%.dev.*}.${i##*.}
+    rm $i
+done
+
+rm -rf .git
+
+rm -rf .gitignore
